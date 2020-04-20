fbe30b018744402487e83e416b779719fee19dae
Adrien <adrien.masson@hotmail.fr>
2020-04-20 16:33:15 +0200

    style login page

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { styles } from "./styles";
-import { View, KeyboardAvoidingView, TouchableOpacity } from "react-native";
-import { Form, Item, Input, Label, Button, Spinner, Text } from "native-base";
+import { View, KeyboardAvoidingView, TextInput, Text, TouchableOpacity, ScrollView } from "react-native";
+
 import useInput from "../../hooks/useInput";
 
 export default function Login({ navigation }) {
@@ -23,9 +23,107 @@ export default function Login({ navigation }) {
 
 	return (
 		<>
-			<KeyboardAvoidingView behavior="padding" style={styles.keyboardAvoiding} enabled>
-				<View style={styles.mainContainer}>
-					{!loading ? (
+			<KeyboardAvoidingView behavior="padding" enabled style={{ backgroundColor: "#FAFAFA", flex: 1 }}>
+				<ScrollView>
+					<View style={{ marginHorizontal: 32, marginVertical: 64 }}>
+						<Text style={{ color: "#413A6C", fontWeight: "bold", fontSize: 32 }}>Log in to your</Text>
+						<Text style={{ color: "#413A6C", fontWeight: "bold", fontSize: 32 }}>account</Text>
+					</View>
+
+					<View style={{ display: "flex", flexDirection: "column", marginHorizontal: 32 }}>
+						<TextInput
+							placeholder="Email"
+							style={{
+								marginVertical: 8,
+								borderRadius: 50,
+								height: 48,
+								backgroundColor: "white",
+								paddingLeft: 24,
+							}}
+						/>
+
+						<View style={{ display: "flex", flexDirection: "row" }}>
+							<View style={{ flex: 4 }}>
+								<TextInput
+									placeholder="Password"
+									style={{
+										marginVertical: 8,
+										borderTopLeftRadius: 50,
+										borderBottomLeftRadius: 50,
+										height: 48,
+										backgroundColor: "white",
+										paddingLeft: 24,
+									}}
+								/>
+							</View>
+							<TouchableOpacity
+								style={{
+									flex: 1,
+									alignItems: "center",
+									justifyContent: "center",
+									borderTopRightRadius: 50,
+									borderBottomRightRadius: 50,
+									backgroundColor: "white",
+									height: 48,
+									marginVertical: 8,
+								}}
+								underlayColor="transparent"
+							>
+								<View>
+									<Text style={{ color: "#413A6C", fontSize: 14 }}>Forgot{"?"}</Text>
+								</View>
+							</TouchableOpacity>
+						</View>
+
+						<TouchableOpacity>
+							<View
+								style={{
+									backgroundColor: "#413A6C",
+									alignItems: "center",
+									justifyContent: "center",
+									borderRadius: 50,
+									marginVertical: 8,
+									height: 48,
+								}}
+							>
+								<Text style={{ color: "white", fontSize: 18, fontWeight: "800" }}>Log in</Text>
+							</View>
+						</TouchableOpacity>
+					</View>
+				</ScrollView>
+
+				<View
+					style={{
+						marginHorizontal: 32,
+						marginBottom: 16,
+						flexDirection: "row",
+						alignItems: "center",
+						justifyContent: "center",
+						flex: 0.1,
+					}}
+				>
+					<Text
+						style={{
+							fontWeight: "500",
+							fontSize: 16,
+							width: 200,
+							textAlign: "center",
+						}}
+					>
+						Don't have an account?
+					</Text>
+					<Text
+						style={{
+							color: "#413A6C",
+							fontWeight: "bold",
+							fontSize: 16,
+							width: 80,
+						}}
+					>
+						Sign up
+					</Text>
+				</View>
+				{/* {!loading ? (
 						<>
 							<Form>
 								<Item floatingLabel style={styles.item}>
@@ -49,17 +147,16 @@ export default function Login({ navigation }) {
 								<Spinner color="white" size="small" />
 							</Button>
 						</View>
-					)}
-				</View>
+					)} */}
 			</KeyboardAvoidingView>
-			<View style={styles.formContainer}>
+			{/* <View style={styles.formContainer}>
 				<TouchableOpacity onPress={() => register()} style={styles.touchableOpacity}>
 					<Text style={styles.formText}>Create an account here</Text>
 				</TouchableOpacity>
 				<TouchableOpacity onPress={() => forgot()} style={styles.touchableOpacity}>
 					<Text style={styles.formText}>Forgot password ?</Text>
 				</TouchableOpacity>
-			</View>
+			</View> */}
 		</>
 	);
 }
