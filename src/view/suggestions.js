19404e9f64ebdaa031c83828e54f94f1e3f5309f
Adrien <adrien.masson@hotmail.fr>
2020-04-25 23:34:22 +0200

    suggestion page

--- src/view/suggestions.js
+++ src/view/suggestions.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { View, KeyboardAvoidingView, Text } from "react-native";
+import { View, KeyboardAvoidingView, Text, Image } from "react-native";
+import { Ionicons, MaterialCommunityIcons, Entypo, FontAwesome5 } from "@expo/vector-icons";
 
 import styles from "../style/view/suggestions";
 
@@ -16,20 +17,9 @@ export default function Suggestions() {
 
 	const title = (firstHalf, secondHalf) => {
 		return (
-			<View style={{ flex: 1, marginTop: 52, marginLeft: 24 }}>
-				<Text style={{ fontSize: 25, fontWeight: "bold", fontFamily: "sans-serif-condensed" }}>
-					{firstHalf}
-				</Text>
-				<Text
-					style={{
-						fontSize: 25,
-						fontWeight: "bold",
-						color: "#5858FA",
-						fontFamily: "sans-serif-condensed",
-					}}
-				>
-					{secondHalf}
-				</Text>
+			<View style={styles.contenaireTitle}>
+				<Text style={styles.title}>{firstHalf}</Text>
+				<Text style={styles.subtitlte}>{secondHalf}</Text>
 			</View>
 		);
 	};
@@ -56,10 +46,60 @@ export default function Suggestions() {
 		}
 	};
 
+	const cardRecipe = () => {
+		return (
+			<View style={styles.containerCard}>
+				<View style={styles.subContainerCard}>
+					<Image style={styles.image} source={require("../assets/burger.jpeg")} />
+					<View style={styles.cardContainerTitle}>
+						<Text style={styles.cardTitle}>Veggie Burger</Text>
+						<Text style={styles.cardSubTitle}>Spicy peanut sauce</Text>
+					</View>
+					<View style={styles.cardFooter}>
+						<Text style={styles.cardFooterText}>420</Text>
+						<Text style={styles.cardFooterSubText}>kcal</Text>
+					</View>
+				</View>
+			</View>
+		);
+	};
+
+	const categoryBar = () => {
+		return (
+			<View style={styles.categoryContainer}>
+				<View style={styles.containerBadge}>
+					<View style={styles.badge}>
+						<Entypo name="book" size={24} color="#8080A5" />
+					</View>
+					<Text style={styles.badgeText}>All</Text>
+				</View>
+				<View style={styles.containerBadge}>
+					<View style={styles.badge}>
+						<FontAwesome5 name="calendar-alt" size={24} color="#8080A5" />
+					</View>
+					<Text style={styles.badgeText}>Daily</Text>
+				</View>
+				<View style={styles.containerBadge}>
+					<View style={styles.badge}>
+						<Ionicons name="md-pizza" size={24} color="#8080A5" />
+					</View>
+					<Text style={styles.badgeText}>Italian</Text>
+				</View>
+				<View style={styles.containerBadge}>
+					<View style={styles.badge}>
+						<MaterialCommunityIcons name="rice" size={24} color="#8080A5" />
+					</View>
+					<Text style={styles.badgeText}>Asian</Text>
+				</View>
+			</View>
+		);
+	};
 	const render = () => {
 		return (
 			<KeyboardAvoidingView behavior="padding" enabled style={styles.keyboardAvoiding}>
 				{displaySentence()}
+				{categoryBar()}
+				{cardRecipe()}
 			</KeyboardAvoidingView>
 		);
 	};
